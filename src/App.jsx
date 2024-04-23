8f4204a6097e3e1d8ac5f32c27a57d4ce050487d
Quang Cường <thanlequangcuong@gmail.com>
2024-04-24 01:28:05 +0700

    basic layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-
 import { useColorScheme } from '@mui/material'
-
 import InputLabel from '@mui/material/InputLabel'
 import MenuItem from '@mui/material/MenuItem'
 import FormControl from '@mui/material/FormControl'
 import Select from '@mui/material/Select'
-
+import { Container } from '@mui/material'
+import Box from '@mui/material'
 
 function ModeSelect() {
   const { mode, setMode } = useColorScheme()
@@ -33,10 +32,37 @@ function ModeSelect() {
 
 function App() {
   return (
-    <>
-      <ModeSelect/>
-      <hr />
-    </>
+    <Container disableGutters maxWidth={false} sx={{ height: '100vh' }}>
+      <Box sx={{
+        backgroundColor: 'primary.light',
+        width: '100%',
+        height: (theme) => theme.trello.appBoardHeight,
+        display: 'flex',
+        alignItems: 'center'
+      }}>
+        <ModeSelect />
+      </Box>
+
+      <Box sx={{
+        backgroundColor: 'primary.dark',
+        width: '100%',
+        height: (theme) => theme.trello.boardBarHeight,
+        display: 'flex',
+        alignItems: 'center'
+      }}>
+        Board Bar
+      </Box>
+      <Box sx={{
+        backgroundColor: 'primary.main',
+        width: '100%',
+        height: (theme) => `calc(100vh - ${theme.trello.appBoardHeight} - ${theme.trello.boardBarHeight})`,
+        display: 'flex',
+        alignItems: 'center'
+      }}>
+        Board Content
+      </Box>
+
+    </Container>
   )
 }
 
